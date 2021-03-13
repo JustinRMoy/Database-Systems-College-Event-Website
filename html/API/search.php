c905dce20e3daf9b9abda4fed5a391fab54502f8
JustinRMoy <justinr2110@gmail.com>
2021-03-13 16:59:26 -0500

    added content headers to db_conn.php and made a search API endpoint, edited the css for events, and made some progress on the js to call the search function

---
+++ html/API/search.php
@@ -0,0 +1,68 @@
+<?php
+
+    require 'db_conn.php';
+
+    $inputFromJson = json_decode(file_get_contents('php://input'), true);
+
+    $Email = $inputFromJson['Email'];
+    $Password = $inputFromJson['Password'];
+
+    //query to DB
+    $sql = "SELECT * FROM Events WHERE (Name like '%" . $inputFromJson['search']. "%' AND Private ='" .$inputFromJson['security']. "')"; 
+    $result = mysqli_query($conn, $sql);
+    $numRows = mysqli_num_rows($result);
+    $resultCount = 0;
+    $searchResult = '';
+
+    //Review SQL Result
+    if($numRows > 0)
+    {
+        //Event found
+        while($eventData = $result->fetch_assoc()){
+                if($resultCount > 0){
+                    $searchResult .= ",";
+                    //seperate search results
+                }
+                $resultCount++;
+                $searchResult .= '{"Name": "' .$eventData["Name"]. '", "Description": "' .$eventData["Description"]. '", "Time": "' .$eventData["Time"]. '", "Date": "' .$eventData["Date"]. '". "eventId": "' .$eventData["ID"]. '"}';
+                //gets data from the searched event
+        }
+        
+        /*if (isset($Users["RSOID"]))
+            $RSO = $Users["RSOID"];
+        */
+        //echo ($id);
+        returnWithInfo($eventData);
+    }
+    //Event not found
+    else
+    {
+        error("No events found");
+    }
+
+    $conn->close();
+
+    //FUNCTIONS
+    function error($err)
+    {
+        $result = '{"user_id":0, "error":"' . $err . '"}';
+        toJSON($result);
+    }
+
+
+    //This return JSON files to JS
+    function toJSON($json)
+    {
+        // "Look JSON"
+        header('Content-type: application/json');
+
+        // "Prints text to the page" 
+        echo $json;
+    }
+    function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":"None"}';
+		toJSON( $retValue );
+	}
+        
+?>
--- html/src/js/events.js
+++ html/src/js/events.js
@@ -1,6 +1,62 @@
-var urlBase = 'https://greengang-gg-4331.com/LAMPAPI';
+var urlBase = '198.199.77.197';
 var extension = 'php';
 
-function createEventCard(){
-    
+function getEvents(query, security)
+{
+	var jsonPayload = '{"search" : "' + query + '", "security" : "' + security + '"}';
+	var url = urlBase + '/API/search.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				var events = JSON.parse( xhr.responseText );
+
+			    if (events.error != "None" && events.error != "No events found")
+				    window.location.href = "Events.html";
+				    
+				var eventList = document.getElementById("eventList");
+				
+				
+				for (var i = 0; i < events.results.length; i++)
+                {
+                    var name = events.results[i].Name;
+                    var description = events.results[i].Description;
+                    var time = events.results[i].Time;
+                    var date = events.results[i].Date;
+                    var eventId = events.results[i].eventId;
+                    var eventCard = createEventCard(name, description, time, date, eventId);
+                    eventList.appendChild(eventCard);
+                }
+                localStorage.setItem("editMode", "false");
+                return;
+			}
+		};
+ 		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		window.location.href = "index.html";
+		return;
+	}
+}
+
+function createEventCard(name, description, time, date, eventId, imgUrl){
+    var eventCard = document.createElement("div");
+    eventCard.className = "row mb-3";
+    eventCard.setAttribute("data-id", eventId);
+
+    var eventImage = document.createElement("div");
+    eventImage.className = "imgPlaceholder";
+    eventImage.setAttribute("id", "eventImg-" + eventId);
+    eventImage.style = "float: left; width: 20%; height: 200px; background-image: url(" + imgUrl + "); box-sizing: border-box; margin-top: 20px; margin-left: 40px; margin-right: 0px; background-repeat: no-repeat; background-position: center;";
+
+    var eventContainer = document.createElement("div");
+    eventContainer.className = "eventsContainer";
+    document.setAttribute("id", "theEvent-" + eventId);
 }
