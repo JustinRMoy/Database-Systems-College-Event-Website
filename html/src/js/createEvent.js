92ae365fdc48cf45683954153fe46d0a52532a93
cjack0416 <70340825+cjack0416@users.noreply.github.com>
2021-03-28 22:03:17 -0400

    fixing up date format and jsonPayload in createEvent.js

--- html/src/js/createEvent.js
+++ html/src/js/createEvent.js
@@ -6,17 +6,19 @@ var uniID;
 
 function readEventInput()
 {   
+    "use strict";
+
     var eventName = document.getElementById("inputEventName").value;
 	var contactEmail = document.getElementById("inputEventEmail").value;
     var contactNumber = document.getElementById("inputEventPhoneNumber").value;
     var description = document.getElementById("inputEventDescription").value;
     var startTime = document.getElementById("inputStartTime").value;
     var endTime = document.getElementById("inputEndTime").value;
-    var startDate = document.getElementById("inputStartDate").value;
-    var endDate = document.getElementById("inputEndDate").value;
+    var startDate = formatDate(document.getElementById("start").value);
+    var endDate = formatDate(document.getElementById("end").value);
     var category = document.getElementById("inputCategory").value;
 		
-        var jsonPayload = '{"EventName" : "' + eventName + '", "Email" : "' + contactEmail + '"Description" : "' + description + '","PhoneNumber" : "' + contactNumber + '", "uniID" : "' + uniID + '", "startTime" : "' + startTime + '", "endTime" : "' + endTime + '", "startDate" : "' + startDate + '", "endDate" : "' + endDate + '", "category" : "' + category + '"}';
+        var jsonPayload = '{"EventName" : "' + eventName + '", "Email" : "' + contactEmail + '", "Description" : "' + description + '", "PhoneNumber" : "' + contactNumber + '", "uniID" : ' + uniID + ', "startTime" : "' + startTime + '", "endTime" : "' + endTime + '", "startDate" : "' + startDate + '", "endDate" : "' + endDate + '", "category" : "' + category + '"}';
 
     	var request = new XMLHttpRequest();
 	    request.open("POST", eventURL, true);
@@ -43,19 +45,18 @@ function readEventInput()
                         document.getElementById("logstatus").style.color = "red";
                     }
                 }
+
+                document.getElementById("inputEventName").value = "";
+                document.getElementById("inputEventEmail").value = "";
+                document.getElementById("inputEventPhoneNumber").value = "";
+                document.getElementById("inputEventDescription").value = "";
+                document.getElementById("inputStartTime").value = "";
+                document.getElementById("inputEndTime").value = "";
+                document.getElementById("start").value = "";
+                document.getElementById("end").value = "";
             }
 
             request.send(jsonPayload);
-            
-            document.getElementById("inputEventName").innerHTML = "";
-            document.getElementById("inputEventEmail").innerHTML = "";
-            document.getElementById("inputEventPhoneNumber").innerHTML = "";
-            document.getElementById("inputEventDescription").innerHTML = "";
-            document.getElementById("inputStartTime").innerHTML = "";
-            document.getElementById("inputEndTime").innerHTML = "";
-            document.getElementById("inputStartDate").innerHTML = "";
-            document.getElementById("inputEndDate").innerHTML = "";
-            document.getElementById("inputCategory").innerHTML = "";
 
         }
 
@@ -64,15 +65,14 @@ function readEventInput()
             document.getElementById("logstatus").innerHTML = "Error info not sent";
             document.getElementById("logstatus").style.color = "red";
 
-            document.getElementById("inputEventName").innerHTML = "";
-            document.getElementById("inputEventEmail").innerHTML = "";
-            document.getElementById("inputEventPhoneNumber").innerHTML = "";
-            document.getElementById("inputEventDescription").innerHTML = "";
-            document.getElementById("inputStartTime").innerHTML = "";
-            document.getElementById("inputEndTime").innerHTML = "";
-            document.getElementById("inputStartDate").innerHTML = "";
-            document.getElementById("inputEndDate").innerHTML = "";
-            document.getElementById("inputCategory").innerHTML = "";
+            document.getElementById("inputEventName").value = "";
+            document.getElementById("inputEventEmail").value = "";
+            document.getElementById("inputEventPhoneNumber").value = "";
+            document.getElementById("inputEventDescription").value = "";
+            document.getElementById("inputStartTime").value = "";
+            document.getElementById("inputEndTime").value = "";
+            document.getElementById("start").value = "";
+            document.getElementById("end").value = "";
 	   }
 	
 }
@@ -106,4 +106,10 @@ function readEventCookie()
 	// {
 	// 	window.location.href = "index.html";
     // }
+}
+
+function formatDate(date)
+{
+    var tokens = date.split("-");
+    return tokens[2] + "-" + tokens[1] + "-" + tokens[0];
 }
