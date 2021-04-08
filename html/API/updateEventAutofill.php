502b0ecfe1dad2c5c99a5d543eeab010eb7ef7be
keaton jones1 <legodude617@gmail.com>
2021-04-08 14:20:57 -0400

    Autofill testing

--- html/API/updateEventAutofill.php
+++ html/API/updateEventAutofill.php
@@ -5,28 +5,18 @@
   $inputFromJson = json_decode(file_get_contents('php://input'), true);
 
   $eventID = $inputFromJson['eventID'];
-  //$uniID = $inputFromJson['uniID'];
-  $eventName = $inputFromJson['EventName'];
-  $email =  $inputFromJson['Email'];
-  $contactNumber = $inputFromJson['PhoneNumber'];
-  $description = $inputFromJson['Description'];
-  $startDate = $inputFromJson['startDate'];
-  $endDate = $inputFromJson['endDate'];
-  $startTime = $inputFromJson['startTime'];
-  $endTime = $inputFromJson['endTime'];
-  $longitude = $inputFromJson['longitude'];
-  $latitude = $inputFromJson['latitude'];
-  $category = $inputFromJson['category'];
 
   $sql = "SELECT Description, contact_num, Contact_Email, startDate, endDate, startTime, endTime, Longitude, Latitude, Category 
           FROM Events 
           Where ID = $eventID";
 
-  if(mysqli_query($conn, $sql))
+  if($query = mysqli_query($conn, $sql))
   {
-    //echo "Event successfully found";
-    echo("Event found is: " + $sql);
-    returnInfo($sql);
+    echo "Event successfully found";
+  // echo $sql;
+  // returnInfo($sql);
+    $autoFill = $query->fetch_assoc();
+    outputJson($autoFill);
   }
   else
   {
@@ -36,21 +26,17 @@
 
   mysqli_close($conn);
     
-  function returnError($error){
-    $retval = '{"msg":"' . $error .'"}';
-    outputJson($retval);
-  }
-  
-  function returnInfo($info)
+  function returnError($error)
   {
-    $retval = '{"msg":"' . $info .'"}';
+    $retval = '{"msg":"' . $error .'"}';
     outputJson($retval);
   }
   
   function outputJson($file)
   {
     header("Content-type:application/json");
-    echo $file;
+    $jsonPackage = json_encode($file);
+    echo $jsonPackage;
   }
   
 ?>
