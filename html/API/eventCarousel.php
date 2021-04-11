b60c7fdfffd5819e685119d876fdd44e1aa44a73
cjack0416 <70340825+cjack0416@users.noreply.github.com>
2021-04-10 20:38:27 -0400

    API endpoint for getting events for carousel in index page and edit in displayEvents.php

---
+++ html/API/eventCarousel.php
@@ -0,0 +1,38 @@
+<?php
+
+    require 'db_conn.php';
+
+    $inputFromJson = json_decode(file_get_contents('php://input'), true);
+
+    $uniID = $inputFromJson['uniID'];
+
+    if ($uniID != NULL)
+        $sql = "SELECT * FROM Events WHERE UniversityID = $uniID ORDER BY ID 
+        DESC LIMIT 4";
+
+    else
+        $sql = "SELECT * FROM Events WHERE Category = 'Public' ORDER BY ID
+        DESC LIMIT 4";
+
+    if ($result = mysqli_query($conn, $sql))
+    {
+        $arr = array();
+
+        while ($row = mysqli_fetch_array($result))
+        {
+            array_push($row, $arr);
+        }
+
+        toJSON($arr);
+    }
+
+    else
+    {
+        var_dump($conn->error);
+    }
+
+    function toJSON($json)
+    {
+        header("Content-type:application/json");
+        echo json_encode($json);
+    }
