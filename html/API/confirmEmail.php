6408f3be067f7451164456c14b676883ffb39e06
MGo01 <matthew.gomez86@gmail.com>
2021-04-13 14:06:43 -0400

    Fixing email verify query

--- html/API/confirmEmail.php
+++ html/API/confirmEmail.php
@@ -3,24 +3,22 @@
   // Connect to database.
   require 'db_conn.php';
 
-  $inputFromJson = json_decode(file_get_contents('php://input'), true);
+  // $inputFromJson = json_decode(file_get_contents('php://input'), true);
 
-  $Email = $inputFromJson['Email'];
   $checkToken = $inputFromJson['emailToken'];
   $verify = 'Y';
 
   // Check if the token references any User in the database.
   $sql = "UPDATE Users SET isVerified = 'Y' WHERE emailToken = 'ed49050180d8e5d8de83131a34afeebc'";
-  $result = mysqli_query($conn, $sql);
 
-  if (mysql_num_rows($result)==1)
+  if (mysqli_query($conn, $sql))
   {
     returnInfo("verified");
   }
 
   else
   {
-    echo("Token may have expired or was not sent properly.");
+    returnError("Token may have expired or was not sent properly.");
   }
 
   mysqli_close($conn);
