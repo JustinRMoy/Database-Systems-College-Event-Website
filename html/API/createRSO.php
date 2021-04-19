9794359461ae34230ff478dfad71f6f7b6055e41
JustinRMoy <justinr2110@gmail.com>
2021-04-18 23:10:23 -0400

    changesTocreateRSO

--- html/API/createRSO.php
+++ html/API/createRSO.php
@@ -16,7 +16,7 @@ foreach($inputFromJson['students'] as $email)
 $uniID = $inputFromJson['uniID'];
 $description = $inputFromJson['description'];
 
-$sql_name = "SELECT * FROM RSO WHERE Name = '$rsoName' AND UniversityID = $uniID";
+$sql_name = "SELECT * FROM RSO WHERE (Name = '" . $rsoName . "' AND UniversityID = " . $uniID . ")";
 $check = mysqli_query($conn, $sql_name);
 
 if ($check == false)
@@ -33,7 +33,7 @@ if ($numRows > 0)
 
 foreach ($students as $student)
 {
-    $sql_id = "SELECT ID from Users WHERE Email = '$student'";
+    $sql_id = "SELECT ID from Users WHERE (Email = '" . $student . "')";
     $result = mysqli_query($conn, $sql_id);
 
     if ($result == false)
@@ -51,7 +51,7 @@ foreach ($students as $student)
 }
 
 $sql_insert = "INSERT INTO RSO (UniversityID, AdminID, Name, Description)
-Values ($uniID, $admin, '".$rsoName."', '".$description."')";
+Values (" .$uniID. ", " .$admin. ", '".$rsoName."', '".$description."')";
 $added = mysqli_query($conn, $sql_insert);
 
 if ($added == false)
@@ -59,7 +59,7 @@ if ($added == false)
     returnError($conn->error);
 }
 
-$sql_update_admin = "UPDATE Users SET User_level = 'Admin' WHERE ID = $admin";
+$sql_update_admin = "UPDATE Users SET User_level = 'Admin' WHERE ID = " .$admin;
 $updated = mysqli_query($conn, $sql_update_admin);
 
 if ($updated == false)
@@ -67,17 +67,17 @@ if ($updated == false)
     returnError($conn->error);
 }
 
-$sql_ros_id = "SELECT ID FROM RSO WHERE Name = '$rsoName' AND UniversityID = $uniID";
+$sql_ros_id = "SELECT ID FROM RSO WHERE Name = '" .$rsoName. "' AND UniversityID = " .$uniID;
 $result = mysqli_query($conn, $sql_ros_id);
 $RSONum = $result->fetch_assoc();
 $id = $RSONum['ID'];
 
-$sql_user_rso = "UPDATE Users SET RSOID = $id WHERE ID = $admin";
+$sql_user_rso = "UPDATE Users SET RSOID = " .$id. " WHERE ID = " .$admin;
 mysqli_query($conn, $sql_user_rso);
 
 foreach($students as $student)
 {
-    $sql_user_rso = "UPDATE Users SET RSOID = $id WHERE Email = '$student'";
+    $sql_user_rso = "UPDATE Users SET RSOID = " .$id. " WHERE Email = '" .$student. "'";
     mysqli_query($conn, $sql_user_rso);
 }
 
