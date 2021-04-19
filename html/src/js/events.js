2fbab268191a82b29ce8a5d81e7628c24d66292c
JustinRMoy <justinr2110@gmail.com>
2021-04-19 04:59:31 -0400

    j

--- html/src/js/events.js
+++ html/src/js/events.js
@@ -254,9 +254,9 @@ function createUserCommentCard(comment, studentId, rating, eventId, commentId){
     cardBody.appendChild(timeStamp);
     //cardBody.appendChild(studentName);
     cardBody.appendChild(commentText);
-    cardBody.appendChild(brk);
     cardBody.appendChild(editButton);
     cardBody.appendChild(deleteButton);
+    cardBody.appendChild(brk);
     /*cardBody.appendChild(social);
     cardBody.appendChild(like); append the buttons here!!!!!!!!!!!!!!!!
     */
