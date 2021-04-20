592b15b8731efaae9201060f2b38d788d0436e04
JustinRMoy <justinr2110@gmail.com>
2021-04-20 13:35:31 -0400

    k

--- html/API/editComments.php
+++ html/API/editComments.php
@@ -1,54 +1,44 @@
 <?php
 
   require 'db_conn.php';
-
+  
   $inputFromJson = json_decode(file_get_contents('php://input'), true);
 
   //$user_level = $inputFromJson['user_level'];
   $sql;
     
   //Only Selects UCF IF for now
-  //$sql_select = "SELECT ID FROM University WHERE Name = 'University of Central Florida'";
+  //$sql_select = "SELECT ID FROM University WHERE Name = 'University of Central Florida';";
   //$result = mysqli_query($conn, $sql_select);
   //$Users = $result->fetch_assoc();
+ 
 
-$userId = $inputFromJson['userId'];
-$eventId = $inputFromJson['eventId'];
-$commentId = $inputFromJson['commentId'];
-$comment = $inputFromJson['comment'];
-$mode = $inputFromJson['mode'];
+if($inputFromJson['mode'] == 1){//1 = create comment
 
-  if ($mode == 1)
-  {
-    //1 = create comment
-    $sql = "INSERT INTO Comments (StudentID, EventID, Comment) VALUES ($userId, $eventId ,'$comment')";
+  $sql = "INSERT INTO Comments (StudentID, EventID, Comment) 
+  VALUES ('". $inputFromJson['userId'] ."','". $inputFromJson['eventId'] ."','". $inputFromJson['comment'] ."')";
 
-  }
-  else if ($mode == 2)
+}elseif($inputFromJson['mode'] == 2){//delete a comment
+  $sql = "DELETE FROM Comments 
+  WHERE CommentID='" . $inputFromJson['commentId'] . "'";
+}elseif($inputFromJson['mode'] == 3){//update a comment
+  $sql = "UPDATE Comments 
+  SET Comment='" . $inputFromJson['comment'] . "'
+  WHERE CommentID='" . $inputFromJson['commentId'] . "'";
+}else{
+  $sql = "INSERT INTO Comments (StudentID, EventID, Comment) 
+  VALUES ('". $inputFromJson['userId'] ."','". $inputFromJson['eventId'] ."','". $inputFromJson['comment'] ."')";
+}
+
+  if($conn->query($sql) != TRUE )
   {
-    //delete a comment
-    $sql = "DELETE FROM Comments 
-    WHERE CommentID=$commentId";
-  }
-  else if ($mode == 3)
-  {
-    //update a comment
-    $sql = "UPDATE Comments SET Comment='$comment' WHERE CommentID=$commentId";
+    echo "SQL Error";
+    returnError( $conn->error );
   }
   else
   {
-    $sql = "INSERT INTO Comments (StudentID, EventID, Comment) VALUES ($userId, $eventId ,'$comment')";  
-    //echo "No mode selected";
-  }
-
-  if (mysqli_query($conn, $sql))
-  {
-     //sendEmail("mr.l.t@hotmail.com");
-     returnInfo("done");
-  }
-  else
-  {
-    returnInfo( "Unable to perform comment operation" );
+    //sendEmail("mr.l.t@hotmail.com");
+    returnInfo("done");
   }
   $conn->close();
     
